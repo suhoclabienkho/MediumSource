3bb87ed51a28242f34d4bcbb65de8052a93a7380
KhanhPQ <suhoclabienkho@gmail.com>
2019-03-20 06:56:50 +0700

    First commit

---
+++ MediumSource/MediumSource.cpp
@@ -0,0 +1,16 @@
+// MediumSource.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "counted_ptr.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+
+	/* Use counted pointer */
+	
+
+	return 0;
+}
